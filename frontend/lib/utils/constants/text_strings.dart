22a38644dd83d663cd4b8df5d8fce4da732b85f0
Intezar-Dev-7 <Intezarcodes302@gmail.com>
2025-03-03 12:26:18 +0530

    first commit

---
+++ frontend/lib/utils/constants/text_strings.dart
@@ -0,0 +1,68 @@
+// This class conatins all the App Text in String formats
+
+class TTexts {
+// Global Text
+  static const String done = 'Done';
+  static const String submit = "Submit";
+  static const String appName = "Eazy-Mart";
+  static const String tContinue = "Continue";
+
+// Onboarding Texts
+  static const String onBoardingTitle1 = 'Choose your product';
+  static const String onBoardingTitle2 = 'Select payment method';
+  static const String onBoardingTitle3 = 'Deliver at your doorstep';
+
+  static const String onBoardingSubTitle1 =
+      'Welcome to the world of limitless Choices';
+  static const String onBoardingSubTitle2 =
+      'For seamless Transactions, Choose your payment Path - Your Convinence , Our Priority !';
+  static const String onBoardingSubTitle3 =
+      'From our doorstep to Yours - Swift , Secure, and Contactless Delivery';
+
+// Authentication Form Text
+  static const String firstName = "First Name";
+  static const String lastName = "Last Name";
+  static const String email = "E-mail";
+  static const String password = "Password";
+  static const String newPassword = "New Password";
+  static const String username = "Username";
+  static const String phoneNo = "Phone Number";
+  static const String rememberMe = "Remember Me";
+  static const String forgotPassword = "Forgot Password";
+  static const String signIn = "Sign In";
+  static const String createAccount = "Create Account";
+  static const String orSignInWith = "or sign in with";
+  static const String orSignUpWith = "or sign up with";
+  static const String iAgreeTo = "I Agree to ";
+  static const String privacyPolicy = "Privacy Policy";
+  static const String termsOfUse = "Terms of use ";
+  static const String verificationCode = "Verification Code";
+  static const String resendEmail = "Resend email";
+  static const String resendEmailIn = "Resend Email in ";
+  static const String and = "and";
+
+  // Authentication Heading Text
+
+  static const String homeAppBarTitle = "Good day for shopping";
+  static const String homeAppBarSubTitle = "Intezar Ali";
+  //
+  static const String loginTitle = "Welcome back, ";
+  static const String loginSubTitle =
+      "Discover limitless Choices and Unmatched Convinience";
+  static const String signupTitle = "Let's create your account";
+  static const String forgotPasswordTitle = "Forgot Password";
+  static const String forgotPasswordSubTitle =
+      "Enter your email and we will send you send you a password reset link";
+  static const String changeYourPasswordTitle = "Password Reset Email Sent";
+  static const String changeYourPasswordSubTitle =
+      "We've sent you  a secure link to safely change your Passowrd adn keep your account protected";
+  static const String confirmEmail = "Verify your email address !";
+  static const String confirmEmailSubTitle =
+      "Congratulations ! Your accounts Awaits : Verify your email to start shopping and experience a world of unrivield Deals and personalized offers";
+
+  static const String emailNotReceiveMessage =
+      "Didn't get the email ? Kindly check your junk/spam folder";
+  static const String yourAccountCreatedTitle = "Account created successfully ";
+  static const String yourAccountCreatedSubTitle =
+      "Your account is created , unleash the joy of seamlessonline shopping";
+}
