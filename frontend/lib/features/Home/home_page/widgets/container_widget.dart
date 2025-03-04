83802f05d225730cb20e2ff7f3883d74e0cf578a
Intezar-Dev-7 <Intezarcodes302@gmail.com>
2025-03-04 15:24:31 +0530

    completed home screen and wallet screen

---
+++ frontend/lib/features/Home/home_page/widgets/container_widget.dart
@@ -0,0 +1,88 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+import 'package:frontend/utils/constants/image_strings.dart';
+
+class ContainerWidget extends StatelessWidget {
+  final String diamondsNumber;
+  final String coinsNumber;
+
+  const ContainerWidget(
+      {super.key, required this.diamondsNumber, required this.coinsNumber});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Expanded(
+        child: Container(
+          padding: EdgeInsets.all(10),
+          // height: 140,
+          // width: 175,
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(224, 226, 252, 1).withOpacity(0.1),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image(
+                    image: AssetImage(diamondImage),
+                    width: 20,
+                  ),
+                  SizedBox(width: 5),
+                  Text(
+                    "Diamonds",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
+                ],
+              ),
+              Container(
+                width: 60,
+                height: 35,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Color.fromRGBO(224, 226, 252, 1).withOpacity(0.1),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image(image: AssetImage(diamondImage), width: 10),
+                    SizedBox(width: 3),
+                    Text(
+                      diamondsNumber,
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image(
+                    image: AssetImage(coinIcon),
+                    width: 10,
+                  ),
+                  SizedBox(width: 5),
+                  Text(
+                    coinsNumber,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- frontend/lib/features/Home/wallet/wallet.dart
+++ frontend/lib/features/Home/wallet/wallet.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/features/Home/tasks/task.dart';
 import 'package:frontend/utils/constants/colors.dart';
+import 'package:frontend/utils/constants/image_strings.dart';
+import 'package:get/get.dart';
 
 class WalletScreen extends StatefulWidget {
   const WalletScreen({super.key});
@@ -21,8 +24,81 @@ class _WalletScreenState extends State<WalletScreen> {
               color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),
         ),
       ),
-      body: Center(
-        child: Image.asset('assets/images/books.png'),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 180,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(224, 226, 252, 1).withOpacity(0.1),
+                borderRadius: BorderRadius.circular(18),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    Row(
+                      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Available coins",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 18),
+                            ),
+                            SizedBox(height: 5),
+                            Row(
+                              // For icon and text
+                              children: [
+                                Image.asset(coinIcon, width: 40),
+                                SizedBox(width: 7),
+                                Text(
+                                  "200",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 40,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 8),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                minimumSize: const Size(349, 40),
+                                // maximumSize: Size.fromWidth(double.infinity),
+                                backgroundColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                              ),
+                              onPressed: () {
+                                Get.offAll(() => TasksScreen());
+                              },
+                              child: Text(
+                                "Earn More",
+                                style: TextStyle(
+                                    color: const Color.fromARGB(255, 19, 2, 49),
+                                    fontSize: 18),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            //
+            SizedBox(height: 25),
+          ],
+        ),
       ),
     );
   }
